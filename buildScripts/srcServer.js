7a49f1b004833fe4afcaccbd14f38c1dc4826839
Dapo Momodu <momoduoladapo@gmail.com>
2019-07-04 15:11:51 +0100

    Enter

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,18 @@
-let express = require('express');
-let path = require('path');
-let open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
-let port = 3000;
-let app = express();
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware') (compiler, {
+  noInfo : true,
+  publicPath : config.output.publicPath
+}));
+
 
 app.get('/', function(req,res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
