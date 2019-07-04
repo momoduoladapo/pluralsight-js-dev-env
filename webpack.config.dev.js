5248ea94a6468d071c6d283c79f9c94d1dc41fe7
Dapo Momodu <momoduoladapo@gmail.com>
2019-07-04 16:51:55 +0100

    Enter

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -22,7 +22,7 @@ export default {
   module: {
     rules: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
-      {test: /\.css$/, loaders: ['style','css']}
+      {test: /\.css$/, loaders: ['style-loader','css-loader']}
     ]
   }
 }
