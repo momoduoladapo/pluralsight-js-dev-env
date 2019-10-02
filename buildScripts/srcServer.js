81d56e255c4dec45e1df614e1ed01b2726940bb9
Dapo Momodu <momoduoladapo@gmail.com>
2019-10-03 00:42:22 +0100

    Enter

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -15,9 +15,17 @@ app.use(require('webpack-dev-middleware') (compiler, {
   publicPath : config.output.publicPath
 }));
 
-
 app.get('/', function(req,res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.get('/users', function(req,res) {
+  // Hard coding for simplicity, Pretend this hits a real database
+  res.json([
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+  ]);
 });
 
 app.listen(port, function(err){
--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import './index.css';
 
-import numeral from 'numeral';
+import {getUsers} from './api/userApi';
 
-const courseValue = numeral(1000).format('$0,0.00');
+//Populate table of all users via API call.
 
-console.log(`I would pay ${courseValue} for this awesome course!`); //eslint-disable-line no-console
+getUsers().then(result => {
+  let usersBody = "";
+
+  result.forEach(user => {
+    usersBody+= `<tr>
+    <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+    <td>${user.id}</td>
+    <td>${user.firstName}</td>
+    <td>${user.lastName}</td>
+    <td>${user.email}</td>
+    </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+});
