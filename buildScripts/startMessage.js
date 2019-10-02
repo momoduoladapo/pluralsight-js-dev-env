59f2e14a4283a75056f0b915c4f45d8b65db3ff5
Dapo Momodu <momoduoladapo@gmail.com>
2019-10-02 22:23:00 +0100

    Enter

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
 import chalk from 'chalk';
 
-console.log(chalk.green('Starting the application in dev mode...'));
+console.log(chalk.green('Starting the application in dev mode...'));  //eslint-disable-line no-console
